e96c3e44507979c7d5ddef8a8c6454545206037b
Nagalinga Rajan <nagalingarajan@gmail.com>
2017-02-19 17:18:18 +0530

    My Django girls app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '++fbnf98gzcpel+@tt^%7amcit&j%supib$w@f!0nfj=p9r!zn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
